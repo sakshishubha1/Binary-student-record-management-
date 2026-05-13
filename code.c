e54bc055a3738763c63bb72441255bc2fe420e91
Shubha Sapkota <shubha.sapkota6b@gmail.com>
2026-05-13 23:49:57 +0545

    update

--- code.c
+++ code.c
@@ -1,1 +1,30 @@
-#include<stdio.h>
+include<stdio.h>
+typedef struct{
+    char name[100];
+    int roll no;
+    int marks ;
+}Student;
+void addstudent(){
+    FILE *fp=fopen("student.dat","ab");
+    if(fp==NULL){
+        printf("EROR OPENING FILE");
+        return 1;
+    }
+    Student s;
+    getchar();
+    printf("Enter name");
+    fgets(s.name,sizeof(s.name),stdin);
+    printf("Enter roll no");
+    scanf("%d",&s.roll);
+    printf("Enter marks",&s.marks);
+    fwrite(&s,sizeof(s),1,fp);
+    fclose(fp);
+    printf("Student added ");
+    }
+
+    int main{
+
+    
+
+    
+}
