5aa776bc7272138587d29dd6b8cf5deedf264537
Shubha Sapkota <shubha.sapkota6b@gmail.com>
2026-05-14 14:55:21 +0545

    update

--- code.c
+++ code.c
@@ -1,8 +1,8 @@
 include<stdio.h>
 typedef struct{
     char name[100];
-    int roll no;
-    int marks ;
+    int roll;
+    int marks;
 }Student;
 void addstudent(){
     FILE *fp=fopen("student.dat","ab");
@@ -21,10 +21,25 @@ void addstudent(){
     fclose(fp);
     printf("Student added ");
     }
+    void viewstudent(){
+        FILE *fp=fopen("student.dat","rb");
+        if(fp==NULL){
+            printf("EROR OPENING FILE");
+            return 1;
+        }
+     printf("displaying students");
+     int Student s1;
+    while(fread(&s1,sizeof(s1),1,fp)==1){
+    printf("%s %d %d",s1.name,s1.roll,s1.marks);
+    printf("--------------------");
+    }
+    fclose(fp);
+}
 
     int main{
 
     
+
 
     
 }
